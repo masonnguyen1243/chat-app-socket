041a779619b912ea43f1403cc124c1becf031e1b
masonnguyen1243 <meowjcisgod@gmail.com>
2025-07-10 15:43:14 +0700

    Message skeleton

--- frontend/src/components/skeletons/MessageSkeleton.jsx
+++ frontend/src/components/skeletons/MessageSkeleton.jsx
@@ -1,4 +1,26 @@
 const MessageSkeleton = () => {
-  return <div>MessageSkeleton</div>;
+  const skeletonMessages = Array(6).fill(null);
+
+  return (
+    <>
+      <div className="flex-1 space-y-4 overflow-y-auto p-4">
+        {skeletonMessages.map((_, index) => {
+          return (
+            <div
+              key={index}
+              className={`flex items-start gap-3 ${index % 2 === 0 ? "justify-start" : "flex-row justify-end"}`}
+            >
+              {/* Avatar */}
+              <div className="h-10 w-10 animate-pulse rounded-full bg-gray-100" />
+
+              {/* Message bubble */}
+              <div className="mb-2 h-4 w-16 animate-pulse rounded bg-gray-300" />
+              <div className="h-16 w-[200px] animate-pulse rounded-lg bg-gray-300" />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 export default MessageSkeleton;
